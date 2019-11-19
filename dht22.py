4a2a4c1ed1bff688292e1806ba7b6484b9194581
Zulsyahmi Othman <44058064+MZulsyahmi@users.noreply.github.com>
2019-11-19 13:55:07 +0800

    Update and rename dht22_test.py to dht22.py

--- dht22.py
+++ dht22.py
@@ -1,3 +1,17 @@
+'''
+---------------------KRENOVATOR---------------------
+  Python for DHT22
+
+  The codes is for control and monitor the DHT22
+  via Python of Raspberry Pi 3B.
+
+  Get the code at github
+  https://github.com/MZulsyahmi/Smart-Air-Quality-Rpi.git
+  
+  by M.Zulsyahmi @krenovator
+  September 2019
+'''
+
 import Adafruit_DHT
 import time
 
